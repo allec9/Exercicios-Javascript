a3aefa1b7cc0cca2c1e858ad98a03f585377f4cb
Allec C. L. <50810927+allec9@users.noreply.github.com>
2024-11-04 08:22:39 -0300

    Add files via upload

--- Ex12/script.js
+++ Ex12/script.js
@@ -1,6 +1,6 @@
-var dolar = Number(prompt("Digite um numero para fazer a conversão de Dólar para Real:"))
-var real = numeroUm * 5.30
+var real = Number(prompt("Digite um numero para fazer a conversão de Real para Dólar:"))
+var dolar = real / 5.30
 
 alert(`
-    A conversão de "R$${numeroUm}" é de "USD$${resultado}"
+    A conversão de "R$${real}" é de "USD$${dolar}"
     `)
