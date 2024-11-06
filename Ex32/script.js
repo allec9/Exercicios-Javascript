425ee3fd7a8ca064fb021c960baa8379e6ea9fb7
Allec C. L. <50810927+allec9@users.noreply.github.com>
2024-11-05 21:17:30 -0300

    Add files via upload

---
+++ Ex32/script.js
@@ -0,0 +1,8 @@
+var peso = Number(prompt("Digite seu peso para descobrir o seu IMC"))
+var altura = Number(prompt("Agora digite sua altura"))
+var imc = (peso / altura) * 2
+
+if(imc < 18.5) {
+    alert("Você está abaixo do peso")
+}
+else if(imc  )
